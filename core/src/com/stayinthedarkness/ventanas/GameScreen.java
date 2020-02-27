f38d71b8550edfcd65462bdab8d2c11deba4ce09
FrankoH298 <francoh298@gmail.com>
2020-02-26 21:07:35 -0300

    added camera movement and zoom

--- core/src/com/stayinthedarkness/ventanas/GameScreen.java
+++ core/src/com/stayinthedarkness/ventanas/GameScreen.java
@@ -1,6 +1,7 @@
 package com.stayinthedarkness.ventanas;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
@@ -18,6 +19,7 @@ public class GameScreen implements Screen {
     private SpriteBatch batch;
     private OrthographicCamera camera;
     private Texture textura;
+    private float velocity;
 
     public GameScreen(StayintheDarkness game) {
         this.game = game;
@@ -33,12 +35,35 @@ public class GameScreen implements Screen {
     public void show() {
     }
 
+    public void update(float delta) {
+        velocity = (1f);
+        //System.out.println(velocity);
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            camera.translate(0, velocity);
+        } else if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            camera.translate(0, -velocity);
+        } else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            camera.translate(velocity, 0);
+        } else if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            camera.translate(-velocity, 0);
+        } else if (Gdx.input.isKeyPressed(Input.Keys.COMMA)) {
+            if (camera.zoom > 0.2f) {
+                camera.zoom -= 0.01f;
+            }
+        } else if (Gdx.input.isKeyPressed(Input.Keys.PERIOD)) {
+            if (camera.zoom < 2f) {
+            camera.zoom += 0.01f;
+            }
+        }
+        camera.update(); // Actualiza la camara.
+    }
+
     @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(1f, 0f, 0f, 1f); // Limpiamos la escena y le establecemos un fondo de color.
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        camera.update(); // Actualiza la camara.
         batch.setProjectionMatrix(camera.combined); // Al batch le va a setear la matriz de la camara.
+        update(delta);
         batch.begin();
         batch.draw(textura, 0, 0);
         batch.end();
