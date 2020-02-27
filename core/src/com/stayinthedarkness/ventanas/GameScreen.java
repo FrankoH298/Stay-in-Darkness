789014fdcc46b775b55ea4b47e7554395ad7834f
FrankoH298 <francoh298@gmail.com>
2020-02-26 21:37:01 -0300

    add FitViewport and method drawText()

--- core/src/com/stayinthedarkness/ventanas/GameScreen.java
+++ core/src/com/stayinthedarkness/ventanas/GameScreen.java
@@ -6,7 +6,9 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.stayinthedarkness.StayintheDarkness;
 
 /**
@@ -20,6 +22,8 @@ public class GameScreen implements Screen {
     private OrthographicCamera camera;
     private Texture textura;
     private float velocity;
+    private BitmapFont font;
+    private FitViewport viewport;
 
     public GameScreen(StayintheDarkness game) {
         this.game = game;
@@ -29,6 +33,9 @@ public class GameScreen implements Screen {
         this.camera = new OrthographicCamera(vpWidth, vpHeight); // Creamos una camara con el tamaño de la ventana.
         textura = new Texture(Gdx.files.internal("default.png"));
         camera.position.set(vpWidth / 2, vpHeight / 2, 0);
+        font = new BitmapFont();
+        viewport = new FitViewport(vpWidth, vpHeight, camera);
+        viewport.apply();
     }
 
     @Override
@@ -36,8 +43,7 @@ public class GameScreen implements Screen {
     }
 
     public void update(float delta) {
-        velocity = (1f);
-        //System.out.println(velocity);
+        velocity = (100f * delta);
         if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
             camera.translate(0, velocity);
         } else if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
@@ -52,7 +58,7 @@ public class GameScreen implements Screen {
             }
         } else if (Gdx.input.isKeyPressed(Input.Keys.PERIOD)) {
             if (camera.zoom < 2f) {
-            camera.zoom += 0.01f;
+                camera.zoom += 0.01f;
             }
         }
         camera.update(); // Actualiza la camara.
@@ -62,15 +68,18 @@ public class GameScreen implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(1f, 0f, 0f, 1f); // Limpiamos la escena y le establecemos un fondo de color.
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.setProjectionMatrix(camera.combined); // Al batch le va a setear la matriz de la camara.
         update(delta);
+        batch.setProjectionMatrix(camera.combined); // Al batch le va a setear la matriz de la camara.
         batch.begin();
         batch.draw(textura, 0, 0);
+        drawText(font, batch, "HOLA PT", 0, 0, 1f, 0.2f, 0.2f, 1f);
         batch.end();
+
     }
 
     @Override
-    public void resize(int i, int i1) {
+    public void resize(int width, int height) {
+        viewport.update(width, height);
     }
 
     @Override
@@ -89,4 +98,8 @@ public class GameScreen implements Screen {
     public void dispose() {
     }
 
+    private void drawText(BitmapFont font, SpriteBatch batch, String text, float x, float y, float r, float g, float b, float a) {
+        font.setColor(r, g, b, a);
+        font.draw(batch, text, camera.position.x + x, camera.position.y + y);
+    }
 }
